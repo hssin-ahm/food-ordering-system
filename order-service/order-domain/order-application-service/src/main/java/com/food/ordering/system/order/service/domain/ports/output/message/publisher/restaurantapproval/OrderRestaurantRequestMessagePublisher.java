f4a83c25a8dea1f866061a52e8ba619fe01ff4d1
Ahmed Hassine <ahm.hssin@gmail.com>
2023-08-12 11:55:52 +0100

    implement input port in order application

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantapproval/OrderRestaurantRequestMessagePublisher.java
+++
@@ -1,7 +0,0 @@
-package com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval;
-
-import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
-import com.food.ordering.system.order.service.domain.event.OrderApproveEvent;
-
-public interface OrderRestaurantRequestMessagePublisher extends DomainEventPublisher<OrderApproveEvent> {
-}
